0754a61ca50995d1cf302343fa216ce0b3eca5b7
joerush18 <joras.aryal23@gmail.com>
2022-12-26 20:33:57 +0545

    Model for api from newsapi response and datas added.

---
+++ app/src/main/java/com/example/easy_news/Models/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.example.easy_news.Models;
+
+import java.util.List;
+
+public class ApiResponse {
+    String status = "";
+    Integer totalResults;
+    List<Articles> articles ;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Articles> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Articles> articles) {
+        this.articles = articles;
+    }
+}
